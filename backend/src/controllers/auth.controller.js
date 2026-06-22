9cbcba5d3ee4dbbb7b41948e4199cff3204b101f
Hemant Bisht <bishthemant169@gmail.com>
2026-06-23 04:03:20 +0530

    fix: update SameSite attribute for JWT cookie to improve security

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -10,8 +10,8 @@ function setCookie(res, token) {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
     secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
 }
 
